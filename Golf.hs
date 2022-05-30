f949a581db6250e5f203653da51f50761ca31e2f
Fugante <ignisacedia@hotmail.com>
2022-05-29 20:00:58 -0500

    WIP: add Golf.hs

---
+++ Golf.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -Wall #-}
+
+
+-- skips "ABCD" == ["ABCD", "BD", "C", "D"]
+-- skips [1] == [[1]]
+-- skips [] == []
+skips :: [a] -> [[a]]
+-- Inner list comprehension takes every nth element of a list.
+skips l = [[x | (x, y) <- zip l [1..], y `mod` n == 0] | n <- [1..length l]]
+
+-- Same as the function above but this uses drop instead of zip to take every nth element.
+-- This version allocates less memory.
+skips' :: [a] -> [[a]]
+skips' l = [
+    every n l |
+    n <- [1..length l],
+    let every m xs = case drop (m - 1) xs of y:ys -> y:every m ys; [] -> []
+    ]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima l = [maximum $ take 2 $ drop n l | n <- [0..length l - 3]]
